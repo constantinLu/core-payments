ebe5720a3582e762b06df0f06c923a427e5f953f
gizet <gizet_st@outlook.com>
2022-11-11 10:46:25 +0200

    ORNG-91 - added swagger

--- src/main/java/com/orange/corepayments/controller/PaymentController.java
+++ src/main/java/com/orange/corepayments/controller/PaymentController.java
@@ -4,6 +4,9 @@ import com.orange.corepayments.client.CorePaymentDto;
 import com.orange.corepayments.client.CorePaymentResponse;
 import com.orange.corepayments.client.PaymentDto;
 import com.orange.corepayments.service.PaymentService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +29,16 @@ public class PaymentController {
         this.paymentService = paymentService;
     }
 
+
+    @ApiOperation(value = "List of payments with statuses", notes = "Receives driver`s payments")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 401, message = "Not authenticated"),
+            @ApiResponse(code = 403, message = "Not authorized"),
+            @ApiResponse(code = 404, message = "Data not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public CorePaymentResponse readPayments(@RequestParam List<String> requestIds) {
         final var uuids = requestIds.stream()
@@ -35,20 +48,39 @@ public class PaymentController {
                 })
                 .collect(Collectors.toList());
 
-
         final var payments = paymentService.findPayments(uuids);
         return CorePaymentResponse.builder()
                 .payments(toCorePaymentDtos(payments))
                 .build();
     }
 
+
+    @ApiOperation(value = "Authorize payment", notes = "Receives an unprocessed payment which will be AUTHORIZED")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 401, message = "Not authenticated"),
+            @ApiResponse(code = 403, message = "Not authorized"),
+            @ApiResponse(code = 404, message = "Data not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @PostMapping
-    public PaymentDto processPayment(@RequestBody CorePaymentDto paymentRequest) {
+    public PaymentDto authorizePayment(@RequestBody CorePaymentDto paymentRequest) {
         Assert.isTrue(paymentRequest.getPaymentStatus().equals(UNPROCESSED), "Payment can only be UNPROCESSED");
         final var authorizePayment = paymentService.authorizePayment(toPayment(paymentRequest));
         return toPaymentDto(authorizePayment);
     }
 
+
+    @ApiOperation(value = "Confirmation of payment", notes = "Receives an authorized payment which will be CONFIRMED")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 401, message = "Not authenticated"),
+            @ApiResponse(code = 403, message = "Not authorized"),
+            @ApiResponse(code = 404, message = "Data not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     @PutMapping
     public PaymentDto confirmPayment(@RequestBody PaymentDto paymentRequest) {
         Assert.isTrue(paymentRequest.getPaymentStatus().equals(PENDING_AUTHORIZATION), "Payment can only be CONFIRMED");
